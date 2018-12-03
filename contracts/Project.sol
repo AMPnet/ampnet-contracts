3ce74d13dc1db1d22bb63d95daca2a8f6ba0a020
Filip Dujmusic <38497167+filipdujmusic@users.noreply.github.com>
2018-12-03 13:38:21 +0100

    Create EUR wallet for deployed project. (#7)
    * Create EUR wallet for deployed project.

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -85,6 +85,10 @@ contract Project {
         _investments[msg.sender] -= amount;
     }
 
+    function getOrganization() public view returns (Organization) {
+        return _organization;
+    }
+
     function getName() public view returns (string) {
         return _name;
     }
