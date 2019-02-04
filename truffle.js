8590e88fef41152dd11ce2e4980b39257a5b5c0b
Filip Dujmusic <filip@ampnet.io>
2019-02-04 14:59:56 +0100

    define two networks: one for testing (0 block time) and one for dev environment (block time > 0)

--- truffle.js
+++ truffle.js
@@ -16,9 +16,14 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
     networks: {
-        development: {
+        dev: {
             host: "127.0.0.1",
             port: 8545,
+            network_id: "*"
+        },
+        test: {
+            host: "127.0.0.1",
+            port: 8546,
             network_id: "*"
         }
     },
