430e88db9a1e28fe193c86279907276a77699aa1
Filip Dujmušić <filip@ampnet.io>
2019-07-19 16:09:29 +0200

    Fixed deployment script setToken issue.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,12 +6,12 @@ module.exports = async function(deployer, network, accounts) {
     const coopOwner = accounts[0];
     const eurTokenOwner = accounts[1];
 
-    deployer.deploy(Cooperative, { from: coopOwner }).then(() => {
-        return deployer.deploy(EUR, Cooperative.address, { from: eurTokenOwner }).then(() => {
-            Cooperative.deployed().then(function(instance) {
-                instance.setToken(EUR.address, { from: coopOwner });
-            })
-        })
-    });
-
+    deployer.deploy(Cooperative, { from: coopOwner })
+        .then(() => Cooperative.deployed())
+        .then((coopInstance) => {
+            return deployer.deploy(EUR, coopInstance.address, { from: eurTokenOwner }).then(() => EUR.deployed())
+                .then((eurInstance) => {
+                    return coopInstance.setToken(eurInstance.address, { from: coopOwner });
+                });
+        });
 };
